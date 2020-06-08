936be4a5540829e42f6f4ba817074caddb3864c1
Luis De Bello <luis.debello@mulesoft.com>
2020-06-08 08:40:01 -0300

    Adding vacuum execution query

---
+++ troubleshooting/vacuum-execution.sql
@@ -0,0 +1,15 @@
+SELECT
+  datname, 
+  usename, 
+  pid, 
+  state, 
+  wait_event, 
+  current_timestamp - xact_start AS xact_runtime, 
+  query
+FROM 
+  pg_stat_activity 
+WHERE 
+  upper(query) LIKE '%VACUUM%' AND
+  pid != pg_backend_pid()
+ORDER BY 
+  xact_start;
